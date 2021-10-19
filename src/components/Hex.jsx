6868f51456ed3548d3ad56b2932a19d47656c23f
Jon Barson <Jon Barson>
2021-10-19 13:08:57 -0400

    functional

--- src/components/Hex.jsx
+++ src/components/Hex.jsx
@@ -1,18 +1,39 @@
-function Hex() {
+function Hex({hex}) {
+  // console.log(translation(hex),hexNumber(hex))
+  const translation = (item) => {
+    if(!item) return
+    const string = `translate(${item[0] * 15 - 5}px, ${item[0] % 2 ? item[1] * 17.32 - 8.66 : item[1] * 17.32}px)`;
+    return {transform: string}
+  }
+/**
+ *
+ * @param {Array} item
+ * @returns the properly formatted hex number
+ */
+  const hexNumber = (item) => {
+    if(!item) return
+    const pad = function(num) {
+      return num.toString().length === 1 ? '0' + num : num;
+    };
+    return pad(item[0]).toString() + pad(item[1]).toString();
+  }
   return (
-    <g ng-attr-transform="translate({{ ::hexGrid.translation(hex) }} )">
+
+    <g style={translation(hex)} onClick={() => console.log(hexNumber(hex))}>
       <polygon
         points="10.0,0 5.0,-8.66 -5.0,-8.66 -10.0,0 -5.0,8.66 5.0,8.66"
         fill="transparent"
         stroke="gray"
-        stroke-width="1"
+        strokeWidth="1"
       ></polygon>
-      <text font-size="5" y="-3" text-anchor="middle" cursor="default">
-        {/* {{ ::hexGrid.hexNumber(hex) }} */}
-        0000
+      <text fontSize="5" y="-3" textAnchor="middle" cursor="default" >
+        {hexNumber(hex)}
       </text>
     </g>
+
   );
 }
 
 export default Hex;
+
+
--- src/components/HexGrid.jsx
+++ src/components/HexGrid.jsx
@@ -4,21 +4,18 @@ function HexGrid({ rows, cols }) {
   const hexes = [];
   const width = (cols / 2) * 30;
   const height = 17.32 * rows + 8.66;
-  for (var j = 1; j <= rows; j++) {
-    for (var i = 1; i <= cols; i++) {
+  for (let j = 1; j <= rows; j++) {
+    for (let i = 1; i <= cols; i++) {
       hexes.push([i, j]);
     }
   }
-  console.log(hexes);
   return (
     <>
       <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
         {hexes.map((hex, index) => (
           <Hex key={index} hex={hex} />
         ))}
-        {/* <hex ng-repeat="hex in ::hexGrid.hexes" hexNum="hex"></hex> */}
       </svg>
-      <Hex />
     </>
   );
 }
